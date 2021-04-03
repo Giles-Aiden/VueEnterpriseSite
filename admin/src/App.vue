9adfca8aff6c01c6a6aac3fd50e7d609472010ad
TheCapn-MEC <erolda266@west-mec.org>
2021-04-02 18:21:05 -0700

    Worked on sidebar
    Need to fix sidebar footer, need to fix logo, possibly space out side bar items

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,32 +1,92 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="hidden" id="sidebar">
+      <vs-sidebar
+        absolute
+        hover-expand
+        v-model="active"
+        open
+        background="primary"
+        textWhite
+        >
+        <template class="logo" #logo>
+          <img id="logoImg" src="@/assets/logo.png" width="auto" height="50rem">
+        </template>
+        <vs-sidebar-item id="home">
+          <template #icon>
+            <i class='bx bx-home'></i>
+          </template>
+          Products
+        </vs-sidebar-item>
+        <br>
+        <vs-sidebar-item id="market">
+          <template #icon>
+            <i class='bx bx-grid-alt'></i>
+          </template>
+          Sales
+        </vs-sidebar-item>
+        <br>
+        <vs-sidebar-item id="Music">
+          <template #icon>
+            <i class='bx bxs-music'></i>
+          </template>
+          Orders
+        </vs-sidebar-item>
+        <br>
+        <vs-sidebar-item id="donate">
+          <template #icon>
+            <i class='bx bxs-donate-heart' ></i>
+          </template>
+          Users
+        </vs-sidebar-item>
+        <template #footer>
+          <vs-row justify="space-between">
+            <table id="sidebar-footer">
+              <tr>
+                <td><h3>David Ray</h3></td>
+                <td><button id="logout">Logout</button></td>
+              </tr>
+            </table>
+          </vs-row>
+        </template>
+      </vs-sidebar>
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+      data:() => ({
+        active: 'home',
+      })
+})
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import 'src/assets/styles/_variables.scss';
+.vs-sidebar__logo {
+  padding: 0;
+}
+img#logoImg {
+  filter: invert(100%);
+  width: 100%;
+  height: 100%;
+  display: block;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+table#sidebar-footer{
+  width: 100%;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  button#logout{
+  background-color: $sidebarElementColor;
+  color: $sidebarTextColor;
+  width: 100%;
+  height: 2rem;
+  padding: 0;
+  border-radius: 1rem;
+  border: none;
+}
 }
 </style>
