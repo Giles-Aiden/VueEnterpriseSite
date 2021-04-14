d9ee8a52bd75819cf27f0b33d4023f213c230d3f
Lily Zalit <lzalit238@west-mec.org>
2021-04-13 17:15:12 -0700

    Basically Nothing :)

---
+++ client/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="center">
+        <vs-card>
+            <template #title>
+                <h3> Professional Laser Engraving </h3>
+            </template>
+        </vs-card>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    export default Vue.extend({
+        
+    })
+</script>
+
+        
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Navbar title="Custom Laser Engraving and Cutting"/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue'
+import HomePage from '@/components/HomePage.vue'
 
 export default {
   name: 'Home',
   components: {
-    Navbar
+    Navbar,
+    HomePage
   }
 }
 </script>
