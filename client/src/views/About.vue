fa43cceeb6cfe1244babcbb9468f63c52dbfd662
Ejtay080 <etayloe242@west-mec.org>
2021-04-15 10:47:57 -0700

    Worked on Collage

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="About">
     <Navbar title="About" />
+    <br>
+    <div class="grid">
+      <vs-row>
+        <vs-col w="4" style="border: solid black 2px;">
+          <img src="" class="blur-collage" style="z-index:1;">
+          <h1 class="collage-text" style="z-index: 2;"></h1>
+        </vs-col>
+        <vs-col w="8" style="border: solid black 2px;">
+          <h1>Gbruh 2</h1>
+        </vs-col>
+      </vs-row>
+      <vs-row>
+        <vs-col w="12" style="border: solid black 2px;">
+          <h1>Gbruh 3</h1>
+        </vs-col>
+      </vs-row>
+      <vs-row>
+        <vs-col w="8" style="border: solid black 2px;">
+          <h1>Gbruh 4</h1>
+        </vs-col>
+        <vs-col w="4" style="border: solid black 2px;">
+          <img src="" class="blur-collage" style="z-index:1;">
+          <h1 style="position:relative;z-index: 2;">Future</h1>
+        </vs-col>
+      </vs-row>
+    </div>
   </div>
 </template>
 
@@ -15,3 +41,22 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.grid{
+  width: 80%;
+  margin: 0 auto;
+  vs-row{
+    align-content: center;
+    text-align: center;
+  }
+
+ 
+}
+collage-text{
+  z-index: 2;
+}
+blur-collage{
+  z-index: 1;
+  filter: blur(50px);
+}
+</style>
