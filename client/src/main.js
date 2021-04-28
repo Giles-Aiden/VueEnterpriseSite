ae6f0adacece170685dc47f25ee2c8da0520f201
KillerHawk-Mec <agiles257@west-mec.org>
2021-04-27 22:46:33 -0700

    index on giles: c527759 Image name fix

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+
 import Vuesax from 'vuesax'
-
 import 'vuesax/dist/vuesax.css' //Vuesax styles
-Vue.use(Vuesax) 
+Vue.use(Vuesax)
 
+import PrimeVue from 'primevue/config';
+Vue.use(PrimeVue);
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+Vue.component('Splitter', Splitter);
+Vue.component('SplitterPanel', SplitterPanel);
 
 Vue.config.productionTip = false
 
