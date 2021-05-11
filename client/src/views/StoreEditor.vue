c60b584e25a94d505ff4b65fbb6616de07986a79
Giles-Aiden <agiles257@west-mec.org>
2021-05-10 17:28:12 -0700

    removed broken file

--- client/src/views/StoreEditor.vue
+++ client/src/views/StoreEditor.vue
@@ -5,21 +5,23 @@
       <vs-button @click="cropToggle('cropToggle')">
         <i id="cropToggle" class="pi pi-sliders-v" />
       </vs-button>
+      <vs-button @click="$refs.editor.undo()"><i class="pi pi-undo" /></vs-button>
+      <vs-button @click="$refs.editor.redo()"><i class="pi pi-refresh" /></vs-button>
+      <input type="color">
+    </div>
+    <div>
       <vs-button @click="$refs.editor.set('selectMode')">
         <i class="pi pi-window-minimize" />
       </vs-button>
-      <vs-button @click="$refs.editor.undo()"
-        ><i class="pi pi-undo"
-      /></vs-button>
-      <vs-button @click="$refs.editor.redo()"
-        ><i class="pi pi-refresh"
-      /></vs-button>
-      <vs-button @click="$refs.editor.clear()"
-        ><i class="pi pi-trash"
-      /></vs-button>
+      <vs-button @click="$refs.editor.set('rect')"> <i class="pi pi-tablet"/> </vs-button>
+      <vs-button @click="$refs.editor.set('circle')"> <i class="pi pi-circle-off"/> </vs-button>
+      <vs-button @click="$refs.editor.set('arrow')"> <i class="pi pi-arrow-circle-up"/> </vs-button>
+      <vs-button @click="$refs.editor.set('freeDrawing')"> <i class="pi pi-pencil"/> </vs-button>
+      <vs-button @click="$refs.editor.clear()"> <i class="pi pi-trash" /> </vs-button>
     </div>
     <div id="editContain">
       <Editor
+        id="editor"
         :canvasWidth="500"
         :canvasHeight="500"
         ref="editor"
@@ -31,7 +33,7 @@
 
 <script>
 // @ is an alias to /src
-//<vs-button @click=""><i class="pi pi-"/></vs-button>
+// <vs-button @click=""><i class="pi pi-"/></vs-button>
 export default {
   name: 'About',
   components: {},
@@ -44,6 +46,7 @@ export default {
   },
   methods: {
     cropToggle: function (id) {
+      console.log('cropple');
       let ele = document.getElementById(id);
       if (ele.classList.contains('pi-sliders-v')) this.$refs.editor.set('crop');
       else this.$refs.editor.applyCropping();
@@ -57,5 +60,9 @@ export default {
 <style lang="scss" scoped>
 .ImageEditor {
   display: flex;
+}
+#editor {
+  width: 20%;
+  height: 20%;
 }
 </style>
