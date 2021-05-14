b6c88e3f72d9bc2ba6ba3edb4ecb497fe83aa60b
Jblack262 <JBlack262@west-mec.org>
2021-05-13 18:15:10 -0700

    started new shop item cards

--- client/src/components/ShopItem.vue
+++ client/src/components/ShopItem.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="shopItem">
-    <vs-card class="itemContain" type="4">
-      <template #title>
-        <h3>{{ itemName }}</h3>
-      </template>
-      <template #img>
-        <img :src="img" :alt="imgAlt" />
-      </template>
-      <template #text>
-        <p>{{ itemBody }}</p>
-      </template>
-    </vs-card>
+    <img class="" :src="img" :alt="imgAlt" srcset="">
+    <h4>{{itemName}}</h4>
+    <p>{{itemBody}}</p>
   </div>
 </template>
 
@@ -29,15 +21,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .shopItem {
-  margin-top: 1em;
-  .itemContain {
-    width: 20rem;
-    height: auto;
-    border: 1em solid #2486f0;
-    border-radius: 2.5em;
-    box-shadow: 20px 20px 30px 10px grey;
-    min-width: 10px;
-    max-width: 80vw;
+  background: lightblue;
+  width: 15rem;
+  height: 15rem;
+  margin: .5rem;
+  position: relative;
+  img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
   }
+
 }
 </style>
