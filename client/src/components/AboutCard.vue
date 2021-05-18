d7d74a43667e723e2d6d7f54b1e62042e705921a
Jblack262 <JBlack262@west-mec.org>
2021-05-17 18:14:59 -0700

    started remaking about page without vuesax

--- client/src/components/AboutCard.vue
+++ client/src/components/AboutCard.vue
@@ -1,110 +1,122 @@
 <template>
-  <div>
-    <div class="big-card">
-      <h1 id="something" class="section-header">Something Here</h1>
-      <p class="big-card-paragraph">Lorem Ipsum</p>
-      <p>
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with
-        desktop publishing software like Aldus PageMaker including versions of
-        Lorem Ipsum.
-      </p>
-      <!--:src="require('@assets/bottleCollage.jpg')"-->
-      <img src="somewhere" />
-      <img src="" />
-    </div>
-
-    <br /><br /><br />
-    <div class="big-card">
-      <h1 id="start" class="section-header">Where We Started</h1>
-      <p class="big-card-paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip
-      </p>
-      <p>
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with
-        desktop publishing software like Aldus PageMaker including versions of
-        Lorem Ipsum.
-      </p>
-      <!--:src="require('@assets/bottleCollage.jpg')"-->
-      <img src="somewhere" />
-      <img src="" />
-    </div>
-    <br /><br /><br />
-    <div class="big-card">
-      <h1 id="journey" class="section-header">Our Journey</h1>
-      <p class="big-card-paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip
-      </p>
-      <p>
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with
-        desktop publishing software like Aldus PageMaker including versions of
-        Lorem Ipsum.
-      </p>
-      <!--:src="require('@assets/bottleCollage.jpg')"-->
-      <img src="somewhere" />
-    </div>
-    <br /><br /><br />
-    <div class="big-card">
-      <h1 id="idea" class="section-header">Idea Behind the Product</h1>
-      <p class="big-card-paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip
-      </p>
-      <p>
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with
-        desktop publishing software like Aldus PageMaker including versions of
-        Lorem Ipsum.
-      </p>
-      <!--:src="require('@assets/bottleCollage.jpg')"-->
-    </div>
-    <br /><br /><br />
-    <div class="big-card">
-      <h1 id="future" class="section-header">Looking to the Future</h1>
-      <p class="big-card-paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip
-      </p>
-      <p>
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ip It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with
-        desktop publishing software like Aldus PageMaker including versions of
-        Lorem Ipsum.
-      </p>
-      <!--:src="require('@assets/bottleCollage.jpg')"-->
-      <img src="somewhere" />
-      <img src="" />
+  <div class="card"><img :src="img" :alt="imgAlt"/>
+    <div class="info">
+      <h1 class="title">{{itemName}}</h1>
+      <p>{{itemBody}}</p>
     </div>
   </div>
-
-  <!--   <div >
-    <div class="big-card" >
-      <h1 id="something" class="section-header">Something</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
-    </div>-->
 </template>
+
+<script>
+export default {
+  name: 'ShopItem',
+  props: {
+    itemName: String,
+    itemBody: String,
+    img: String,
+    imgAlt: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.card {
+  width: 340px;
+  height: 250px;
+  border-radius: 15px;
+  margin-block: 0.5rem;
+  padding: 1.5rem;
+  background: white;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  transition: 0.8s ease-out;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
+}
+.card:hover:before,
+.card:focus:before {
+  opacity: 1;
+}
+.card:hover .info,
+.card:focus .info {
+  opacity: 1;
+  transform: translateY(-140px);
+  p {
+    opacity: 1;
+  }
+  button {
+    opacity: 1;
+  }
+}
+.card:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 2;
+  transition: 0.8s;
+  opacity: 0;
+}
+.card img {
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+     object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 15px;
+}
+.card .info {
+  position: relative;
+  margin: 0 auto;
+  z-index: 3;
+  color: white;
+  transform: translateY(30px);
+  transition: 0.8s;
+  .title {
+    opacity: 1 !important;
+    text-shadow:
+      -1px -1px 0 #000,  
+      1px -1px 0 #000,
+      -1px 1px 0 #000,
+      1px 1px 0 #000;
+  }
+  p {
+    opacity: 0;
+    transition: 0.8s;
+  }
+  button {
+    opacity: 0;
+  }
+}
+.card .info h1 {
+  margin: 0px;
+}
+.card .info p {
+  letter-spacing: 1px;
+  font-size: 15px;
+  margin-top: 8px;
+}
+.card .info button {
+  padding: 0.6rem;
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  background: white;
+  color: black;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.8s ease;
+}
+</style>
