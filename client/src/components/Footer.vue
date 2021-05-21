d1f74e2d1ac39469eadb9c8cd82602f8cf531e7b
Jblack262 <JBlack262@west-mec.org>
2021-05-20 18:02:47 -0700

    updated footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,43 +1,103 @@
 <template>
-  <div class="center">
-    <div>
-      <h1>Contact Information</h1>
+  <div class="center background">
+    <div class="contact item">
+      <h3><i class="fa fa-user-circle"></i>- David Ray</h3>
+      <h3><i class="fa fa-phone-square"></i>- (123)-456-7890</h3>
+      <h3><i class="fa fa-envelope"></i>- sales@WFBMlaserengraving.com</h3>
     </div>
-    <div class="icons">
-      <h1>Social Media</h1>
-      <vs-button icon color="facebook">
+    <div class="logo item">
+      <img src="@/assets/WFBM Logo.png" alt="">
+    </div>
+    <div class="icons item">
+      <vs-button
+        class="button-icon"
+        icon
+        color="facebook"
+        href="http://business.facebook.com/WFBM-Laser-Engraving-102699948274177/"
+      >
         <i class="fa fa-facebook-square"></i>
       </vs-button>
 
-      <vs-button icon color="twitter">
-        <i class="fa fa-twitter-square"></i>
-      </vs-button>
+      <div class="icon">
+        <vs-button class="button-icon" icon color="twitter">
+          <i class="fa fa-twitter-square"></i>
+          <!--@LaserWfbm-->
+        </vs-button>
+      </div>
 
-      <vs-button icon color="linkedin">
+      <vs-button class="button-icon" icon color="linkedin">
         <i class="fa fa-linkedin-square"></i>
       </vs-button>
 
-      <vs-button icon color="google-plus">
-        <i class="fa fa-google-plus-square"></i>
-      </vs-button>
-
-      <vs-button icon color="pinterest">
-        <i class="fa fa-pinterest-square"></i>
-      </vs-button>
+      <div class="icon">
+        <vs-button class="button-icon" icon color="instagram">
+          <i class="fa fa-instagram"></i>
+          <!--@wfbmlaserengraving-->
+        </vs-button>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-ul {
-  background: red;
+<script>
+export default {
+  name: 'footer',
 }
+</script>
+
+<style lang="scss" scoped>
+$shadow: grey;
+$text-color: white;
+
 .center {
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .contact {
+    text-align: left;
+  }
+  .background {
+    background-image: url('../assets/navBackgroundBottles.png');
+    opacity: 20%;
+
+  }
+  background-size: cover;
+  background-position: center;
+}
+.item {
+  flex: 1 1 0px;
+  justify-content: center;
+  align-items: center;
+  margin-inline: 1rem;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    margin-block: 1rem;
+    max-width: 100%;
+    min-width: 250px;
+    max-height: 100px;
+  }
 }
 .icons {
   display: flex;
-  float: right;
+  .icon {
+    display: flex;
+  }
+}
+
+.fa {
+  padding: 0.5rem;
+}
+
+.button-icon {
+  max-height: 2rem;
+}
+
+.center {
+  background-color: $shadow;
+  color: $text-color;
 }
 </style>
