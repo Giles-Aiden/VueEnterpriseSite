5d6f13fa8f4bf3b5eb558664b06b4893c648d0cb
Jblack262 <JBlack262@west-mec.org>
2021-05-20 17:20:29 -0700

    finished footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,46 +1,39 @@
 <template>
   <div class="center background">
-    <div>
-      <h1>Contact Information</h1>
+    <div class="contact item">
       <h3>David Ray- Owner</h3>
       <h3>Phone- (480)-226-6529</h3>
       <h3>Email- Sales@WFBMlaserengraving.com</h3>
     </div>
-      <div>
-      </div>
-    <div>
+    <div class="logo item">
+      <img src="@/assets/WFBM Logo.png" alt="">
+    </div>
+    <div class="icons item">
+      <vs-button
+        class="button-icon"
+        icon
+        color="facebook"
+        href="http://business.facebook.com/WFBM-Laser-Engraving-102699948274177/"
+      >
+        <i class="fa fa-facebook-square"></i>
+      </vs-button>
 
-      <div>
-        <h1>Social Media</h1>
-      </div>
-
-      <div class="icons">
-        <vs-button
-          class="button-icon"
-          icon
-          color="facebook"
-          href="http://business.facebook.com/WFBM-Laser-Engraving-102699948274177/"
-        >
-          <i class="fa fa-facebook-square"></i>
+      <div class="icon">
+        <vs-button class="button-icon" icon color="twitter">
+          <i class="fa fa-twitter-square"></i>
+          <!--@LaserWfbm-->
         </vs-button>
+      </div>
 
-        <div class="icon">
-          <vs-button class="button-icon" icon color="twitter">
-            <i class="fa fa-twitter-square"></i>
-            <!--@LaserWfbm-->
-          </vs-button>
-        </div>
+      <vs-button class="button-icon" icon color="linkedin">
+        <i class="fa fa-linkedin-square"></i>
+      </vs-button>
 
-        <vs-button class="button-icon" icon color="linkedin">
-          <i class="fa fa-linkedin-square"></i>
+      <div class="icon">
+        <vs-button class="button-icon" icon color="instagram">
+          <i class="fa fa-instagram"></i>
+          <!--@wfbmlaserengraving-->
         </vs-button>
-
-        <div class="icon">
-          <vs-button class="button-icon" icon color="instagram">
-            <i class="fa fa-instagram"></i>
-            <!--@wfbmlaserengraving-->
-          </vs-button>
-        </div>
       </div>
     </div>
   </div>
@@ -56,18 +49,34 @@ ul {
 .center {
   display: flex;
   justify-content: space-around;
+  flex-wrap: wrap;
   .background {
     background-image: url('../assets/navBackgroundBottles.png');
     opacity: 20%;
+
   }
   background-size: cover;
   background-position: center;
 }
-
+.item {
+  flex: 1 1 0px;
+  justify-content: center;
+  align-items: center;
+  margin-inline: 1rem;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    margin-block: 1rem;
+    max-width: 100%;
+    min-width: 250px;
+    max-height: 100px;
+  }
+}
 .icons {
   display: flex;
-  float: right;
-  flex-wrap: wrap;
   .icon {
     display: flex;
   }
