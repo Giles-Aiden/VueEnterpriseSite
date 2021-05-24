c6d3890c906ebc2c1f9072111f4a96199d30a916
KillerHawk-Mec <agiles257@west-mec.org>
2021-05-24 12:39:38 -0700

    payments

--- client/src/views/StoreEditor.vue
+++ client/src/views/StoreEditor.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="ImageEditor">
+  <div class="StoreEditor">
     <div>
-      <vs-button><i class="pi pi-upload" /></vs-button>
       <vs-button class="button-color" @click="cropToggle('cropToggle')">
         <i id="cropToggle" class="pi pi-sliders-v" />
       </vs-button>
@@ -29,8 +28,8 @@
       <vs-button @click="$refs.editor.set('freeDrawing')">
         <i class="pi pi-pencil" />
       </vs-button>
-      <vs-button @click="$refs.editor.clear()">
-        <i class="pi pi-trash" />
+      <vs-button @click="$refs.editor.set('eraser')">
+        <i class="pi pi-times" />
       </vs-button>
       <vs-button @click="$refs.editor.undo()">
         <i class="pi pi-undo" />
@@ -50,6 +49,7 @@
         ref="editor"
         editorId="canvasId"
       />
+      <input type="file" accept="image/*" @change="uploadImage($event)" />
     </div>
   </div>
 </template>
@@ -58,10 +58,12 @@
 // @ is an alias to /src
 // <vs-button @click=""><i class="pi pi-"/></vs-button>
 export default {
-  name: 'About',
+  name: 'StoreEditor',
   components: {},
   mounted() {
-    this.$refs.editor.setBackgroundImage(require('@/assets/logo.png'));
+    this.$refs.editor.clear();
+    this.$refs.editor.setBackgroundImage(require('@/assets/logo.png'), '');
+    this.$refs.editor.filter('grayscale(100%)');
     this.$refs.editor.set(this.$refs.editor.mode, this.$refs.editor.options);
   },
   data() {
@@ -76,6 +78,49 @@ export default {
       ele.classList.toggle('pi-sliders-v');
       ele.classList.toggle('pi-check-square');
     },
+    uploadImage: function (e) {
+      let canvas = this.$refs.editor;
+      let context = canvas.getContext('2d');
+      let currentX = canvas.width / 2;
+      let currentY = canvas.height / 2;
+      e.onload = () => {
+        //mouse events
+        let isDraggable;
+        canvas.onmousedown = (x) => {
+          let mouseX = x.pageX - this.offsetLeft;
+          let mouseY = x.pageY - this.offsetTop;
+          if (
+            mouseX >= currentX - e.width / 2 &&
+            mouseX <= currentX + e.width / 2 &&
+            mouseY >= currentY - e.height / 2 &&
+            mouseY <= currentY + e.height / 2
+          ) {
+            isDraggable = true;
+          }
+        };
+        canvas.onmouseup = () => {
+          isDraggable = false;
+        };
+        canvas.onmouseout = () => {
+          isDraggable = false;
+        };
+        canvas.onmousemove = function (x) {
+          if (isDraggable) {
+            currentX = x.pageX - this.offsetLeft;
+            currentY = x.pageY - this.offsetTop;
+          }
+        };
+
+        setInterval(() => {
+          //Reset Canvas
+          context.fillStyle = '';
+          context.fillRect(0, 0, canvas.width, canvas.height);
+          //Draw Image
+          context.drawImage(e, currentX - e.width / 2, currentY - e.height / 2);
+        }, 1000 / 30);
+      };
+      this.$refs.editor.uploadImage(e);
+    },
   },
 };
 </script>
@@ -89,11 +134,13 @@ $shadow: #113f70;
   background-color: $bg-main;
   box-shadow: $shadow;
 }
-.ImageEditor {
+.StoreEditor {
   display: flex;
+  align-content: space-around;
 }
 #editor {
-  width: 20%;
-  height: 20%;
+  border: 1em inset black;
+  background-image: url('../assets/bottles/flask_white.png');
+  background-size: 100%;
 }
 </style>
--- client/src/views/StorePayment.vue
+++ client/src/views/StorePayment.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="Payment"></div>
+  <div class="Payment">
+    <stripe-checkout
+      ref="checkout"
+      mode="payment"
+      :pk="stripePK"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="(v) => (loading = v)"
+    />
+    <vs-button @click="submit">Pay!</vs-button>
+  </div>
 </template>
 
 <script>
+import { StripeCheckout } from '@vue-stripe/vue-stripe';
 export default {
   name: 'Payment',
-  components: {},
+  components: {
+    StripeCheckout,
+  },
+  methods: {
+    submit() {
+      // You will be redirected to Stripe's secure checkout page
+      this.$refs.checkout.redirectToCheckout();
+    },
+  },
+  data() {
+    this.stripePK = process.env.VUE_APP_STRIPE_PK;
+    return {
+      loading: false,
+      lineItems: [
+        {
+          price: 'price_1IuhWZAoLvRfzUcy4ifJ1Ds3', // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: `${process.env.VUE_APP_DOMAIN}/payment-success`,
+      cancelURL: `${process.env.VUE_APP_DOMAIN}/payment-cancel`,
+    };
+  },
 };
 </script>
 
