9255cb36d6d9db33b0e69267a258afd9577b2d0b
Giles-Aiden <agiles257@west-mec.org>
2021-07-20 14:26:52 -0700

    vue config

---
+++ admin/vue.config.js
@@ -0,0 +1,14 @@
+// vue.config.js
+module.exports = {
+  // options...
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
+/*
+  devServer: {
+    proxy: 'https://piwigo.wfbmlaserengraving.com/',
+  },
+*/
+};
